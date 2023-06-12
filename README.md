a7dc4eb2f8f57ea1aa855c31d37a8c700059c456
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-06-12 10:31:52 +0200

    password generator done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# full-template
-Base html e css
+<!-- ESERCIZIO PASSWORD GENERATOR -->
+
+1. Chiedi all'utente il suo nome,
+2. poi chiedi il suo cognome,
+3. poi chiedi il suo colore preferito
+4. Infine scrivi sulla pagina nomecognomecolorepreferito101 
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//  1)
+const userFirstName = prompt('Il tuo nome: '); 
+
+console.log('User name',userFirstName, typeof userFirstName);
+
+
+//  2)
+const userSecondName = prompt('Il tuo cognome: '); 
+
+console.log('User name',userSecondName, typeof userSecondName);
+
+
+// 3)
+const userColor = prompt('Il tuo colore preferito: '); 
+
+console.log('User color',userColor, typeof uuserColor);
+
+// 4)
+const pwd = userFirstName + userSecondName + userColor + '101';
+
+console.log('pwd', pwd, typeof pwd);
+
+
+
+
+
+
+const pwdContainer = document.getElementById('pwd-container')
+
+console.log('pwdContainer', pwdContainer, typeof pwdContainer)
+
+pwdContainer.innerHTML = 'LA TUA PASSSWORD'
